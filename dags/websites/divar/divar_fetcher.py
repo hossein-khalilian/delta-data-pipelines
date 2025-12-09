b53033a33b0665c9a577cff521b8c54d3eaad5e5
hossein khalilian <hse.khalilian08@gmail.com>
2025-12-09 17:23:45 +0330

    make divar fetcher sync

--- dags/websites/divar/divar_fetcher.py
+++ dags/websites/divar/divar_fetcher.py
@@ -1,34 +1,28 @@
 import asyncio
+
 import httpx
 
-# fetcher_function
-def fetcher_function(messages):
 
+def fetcher_function(messages):
     if not messages:
         print("No messages available from Sensor.")
         return []
 
-    async def fetch_all(messages):
-        async with httpx.AsyncClient(verify=True) as client:
-            fetched = []
-            for index, msg in enumerate(messages, start=1):
-                print(index)
-                
-                url = msg["content_url"]
-                
-                try:
-                    resp = await client.get(url)
-                    resp.raise_for_status()
-                    fetched.append({"content_url": url, "data": resp.json()})
-                    await asyncio.sleep(2)
-                except Exception as e:
-                    print(f"Fetch error {url}: {e}")
-            return fetched
+    fetched = []
 
-    fetched_data = asyncio.run(fetch_all(messages))
-    if fetched_data:
-        print(f"✅ Processed {len(fetched_data)} items")
-    else:
-        print("No data fetched from API.")
-    return fetched_data
+    with httpx.Client(verify=True) as client:
+        for index, msg in enumerate(messages, 1):
+            print(index)
+            url = msg["content_url"]
+            try:
+                resp = client.get(url)
+                resp.raise_for_status()
+                fetched.append({"content_url": url, "data": resp.json()})
+                time.sleep(2)
+            except Exception as e:
+                print(f"Fetch error {url}: {e}")
 
+    print(
+        f"✅ Processed {len(fetched)} items" if fetched else "No data fetched from API."
+    )
+    return fetched
